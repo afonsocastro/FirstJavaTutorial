c3e405c7e9697301df6416bf7ccd4e571d155bc2
AfonsoCastro <afonsocastro@ua.pt>
2020-01-16 16:41:52 +0000

    First Java Tutorial

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    public static String iAmDog() {
+        return "I am a dog";
+    }
+
+    public static void  doStuff() {
+        int a = 5;
+        while ( a < 50){
+            System.out.println("hi!");
+            a++;
+        }
+    }
+}
---
+++ src/LearnJava.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class LearnJava {
+    public static void  main(String[] args) {
+//        Animal a = new Animal();
+//        String dog = a.iAmDog();
+//        System.out.println(dog);
+//        ArrayList<Integer> a = new ArrayList<Integer>();
+        Animal.doStuff();
+    }
+}
